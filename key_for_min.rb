181d342e9055982e6c2faf9622ebd41fbe0618d7
Jessy Hu <jesshuy12@gmail.com>
2019-02-03 19:40:06 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,5 +7,6 @@ def key_for_min_value(name_hash)
   
   name_hash.collect do |key, value|
     if min_value == nil || value < min_value
-      min
+      min_value = value
+      min_key = key
       
